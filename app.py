0fd087496d7c41c44da94a9578a063817057e8b1
RANYAKETFI <47351011+RANYAKETFI@users.noreply.github.com>
2022-06-15 16:36:04 +0200

    Update app.py

--- app.py
+++ app.py
@@ -117,7 +117,9 @@ def forcast():
           
           f = request.files['data']
           f.save(secure_filename(f.filename))
-          df=pd.read_csv(f.filename)
+          with open(f.filename) as csv_file:
+                 df = csv.read_csv(csv_file, delimiter=',')
+          #df=pd.read_csv(f.filename)
           chosen_file=f.filename
           if len(df.columns)==2 : 
            col1=df.columns[0]
