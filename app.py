e03c9a1a3c7a7adf95f89f901a15eb6e2097d3a9
RANYAKETFI <47351011+RANYAKETFI@users.noreply.github.com>
2022-06-15 11:59:16 +0200

    Update app.py

--- app.py
+++ app.py
@@ -40,8 +40,8 @@ def load_user(user_id):
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), nullable=False, unique=True)
-    password = db.Column(db.String(80), nullable=False)
+    username = db.Column(db.String(200), nullable=False, unique=True)
+    password = db.Column(db.String(200), nullable=False)
 
 
 
